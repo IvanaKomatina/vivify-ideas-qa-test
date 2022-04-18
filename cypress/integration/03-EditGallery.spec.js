e698adcdc1826128a0120e4219b896eb3284fccf
IvanaKomatina <komatinaivana@yahoo.com>
2022-04-18 15:47:01 +0200

    Edit gallery scenario finished

---
+++ cypress/integration/03-EditGallery.spec.js
@@ -0,0 +1,94 @@
+import { loginPage } from "../pageObject/loginPage.js"
+import { navigation } from "../pageObject/navigation.js"
+import user from "../fixtures/user.json"
+import { galleries } from "../pageObject/galleries.js"
+
+describe('Edit Gallery', () => {
+    beforeEach(() => {
+        cy.generateFixture()
+        cy.visit('')
+        navigation.LoginBtn.click()
+        loginPage.Login(user.email,user.password)
+    })
+
+    it('Edit gallery without title', () => {
+            navigation.MyGalleriesBtn.click()
+            galleries.FirstImage.click()
+            galleries.EditGalleryBtn.click()
+            galleries.EditGalleryTitle.should('have.text', 'Edit Gallery')
+            galleries.TrashBtn.eq(1).click()
+            galleries.TitleInput.clear()
+            galleries.Gallery("" , "" , "")
+            galleries.TitleInput.then(($input) => {
+                expect($input[0].validationMessage).to.eq('Please fill out this field.')
+            })
+
+    })
+
+    it('Edit gallery without url', () => {
+        navigation.MyGalleriesBtn.click()
+        galleries.FirstImage.click()
+        galleries.EditGalleryBtn.click()
+        galleries.TrashBtn.eq(1).click()
+        galleries.ImageInput.clear()
+        galleries.Gallery("", "", "")
+        galleries.ImageInput.then(($input) => {
+            expect($input[0].validationMessage).to.eq('Please fill out this field.')
+        })
+    })
+
+    it('Edit gallery without valid image extension', () => {
+        navigation.MyGalleriesBtn.click()
+        galleries.FirstImage.click()
+        galleries.EditGalleryBtn.click()
+        galleries.TrashBtn.eq(1).click()
+        galleries.ImageInput.clear()
+        galleries.Gallery("", "" , "https://krstarica.name/wp-content/uploads/2021/05/pas-1-750x430.com")
+        galleries.ErrorMessage.should('have.text', 'Wrong format of image')
+    })
+
+    it('Edit gallery with invalid url', () => {
+        navigation.MyGalleriesBtn.click()
+        galleries.FirstImage.click()
+        galleries.EditGalleryBtn.click()
+        galleries.TrashBtn.eq(1).click()
+        galleries.ImageInput.clear()
+        galleries.Gallery("" , "" , "www.google.com")
+        galleries.ImageInput.then(($input) => {
+            expect($input[0].validationMessage).to.eq('Please enter a URL.')
+        })
+    })
+
+    it('Edit gallery without title and url', () => {
+        cy.fixture('gallery').then(galleryData => {
+            navigation.MyGalleriesBtn.click()
+            galleries.FirstImage.click()
+            galleries.EditGalleryBtn.click()
+            galleries.TrashBtn.eq(1).click()
+            galleries.TitleInput.clear()
+            galleries.ImageInput.clear()
+            galleries.Gallery("" , galleryData.Description, "")
+            galleries.TitleInput.then(($input) => {
+                expect($input[0].validationMessage).to.eq('Please fill out this field.')
+            })
+        })
+    })
+
+    // Ovaj test case pada jer smatram da je ovo bug
+    // jer dopusta kreiranje galerije sa slikom koja
+    // ne postoji, a ocekivano je da izbaci gresku da
+    // slika ne postoji
+    it('Edit gallery with no existing image', () => {
+        cy.fixture('gallery').then(galleryData => {
+            navigation.MyGalleriesBtn.click()
+            galleries.FirstImage.click()
+            galleries.EditGalleryBtn.click()
+            galleries.TrashBtn.eq(1).click()
+            galleries.TitleInput.clear()
+            galleries.DescriptionInput.clear()
+            galleries.ImageInput.clear()
+            galleries.Gallery(galleryData.Title, galleryData.Description, "https://www.google.com.jpg")
+            galleries.ErrorMessage.should('have.text', 'The image does not exist.')
+        })
+    })
+})
